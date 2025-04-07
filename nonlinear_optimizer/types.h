0edc4d393260a53111909bd8d52a234c86e5178f
ChanghyeonKim93 <hyun91015@gmail.com>
2025-04-08 01:50:03 +0900

    add definitions of single precision vectors and matrices

--- nonlinear_optimizer/types.h
+++ nonlinear_optimizer/types.h
@@ -13,6 +13,7 @@ using Vec6 = Eigen::Matrix<double, 6, 1>;
 
 using Mat2x2 = Eigen::Matrix<double, 2, 2>;
 using Mat2x3 = Eigen::Matrix<double, 2, 3>;
+using Mat3x2 = Eigen::Matrix<double, 3, 2>;
 using Mat3x3 = Eigen::Matrix<double, 3, 3>;
 using Mat4x4 = Eigen::Matrix<double, 4, 4>;
 using Mat5x5 = Eigen::Matrix<double, 5, 5>;
@@ -27,6 +28,30 @@ using Mat6x1 = Eigen::Matrix<double, 6, 1>;
 
 using Orientation = Eigen::Quaterniond;
 using Pose = Eigen::Isometry3d;
+
+using Vec2f = Eigen::Matrix<float, 2, 1>;
+using Vec3f = Eigen::Matrix<float, 3, 1>;
+using Vec4f = Eigen::Matrix<float, 4, 1>;
+using Vec5f = Eigen::Matrix<float, 5, 1>;
+using Vec6f = Eigen::Matrix<float, 6, 1>;
+
+using Mat2x2f = Eigen::Matrix<float, 2, 2>;
+using Mat2x3f = Eigen::Matrix<float, 2, 3>;
+using Mat3x2f = Eigen::Matrix<float, 3, 2>;
+using Mat3x3f = Eigen::Matrix<float, 3, 3>;
+using Mat4x4f = Eigen::Matrix<float, 4, 4>;
+using Mat5x5f = Eigen::Matrix<float, 5, 5>;
+using Mat6x6f = Eigen::Matrix<float, 6, 6>;
+
+using Mat1x6f = Eigen::Matrix<float, 1, 6>;
+using Mat2x6f = Eigen::Matrix<float, 2, 6>;
+using Mat3x6f = Eigen::Matrix<float, 3, 6>;
+using Mat6x3f = Eigen::Matrix<float, 6, 3>;
+using Mat6x2f = Eigen::Matrix<float, 6, 2>;
+using Mat6x1f = Eigen::Matrix<float, 6, 1>;
+
+using Orientationf = Eigen::Quaternionf;
+using Posef = Eigen::Isometry3f;
 
 }  // namespace nonlinear_optimizer
 
